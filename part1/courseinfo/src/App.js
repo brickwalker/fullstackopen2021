6e5939b6ea9aae24276a03e563d61907ea655e8b
Artem Nedostup <artem.nedostup@gmail.com>
2021-12-01 15:19:16 +0000

    Exercises 1.3.-1.5.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const contentData = [
-    { part: "Fundamentals of React", exercises: 10 },
-    { part: "Using props to pass data", exercises: 7 },
-    { part: "State of a component", exercises: 14 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { part: "Fundamentals of React", exercises: 10 },
+      { part: "Using props to pass data", exercises: 7 },
+      { part: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header courseName={course} />
-      <Content data={contentData} />
-      <Total data={contentData} />
+      <Header courseName={course.name} />
+      <Content data={course.parts} />
+      <Total data={course.parts} />
     </div>
   );
 };

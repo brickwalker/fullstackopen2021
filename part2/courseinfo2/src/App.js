ff2842f98827aba9aed69dbbf96bd0950a16d475
brickwalker <artem.nedostup@gmail.com>
2021-12-04 10:15:46 +0000

    2.5: separate module

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,35 +1,5 @@
 import React from "react";
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>;
-};
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>
-      <strong>Total of {sum} exercises</strong>
-    </p>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
@@ -80,17 +50,9 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {courses.map(course => <Course key={course.id} course={course} />)}
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };

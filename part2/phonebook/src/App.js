95b05727c12eba1c02c999c06c3164bf4c878a4e
brickwalker <artem.nedostup@gmail.com>
2021-12-04 15:07:33 +0000

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: 11111 }]);
+  const [name, setNewName] = useState("");
+  const [phone, setNewPhone] = useState("");
 
   const handleNameEntry = (event) => setNewName(event.target.value);
+  const handlePhoneEntry = (event) => setNewPhone(event.target.value);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (persons.find((element) => element.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    if (persons.find((element) => element.name === name)) {
+      alert(`${name} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name, phone }]);
     }
     setNewName("");
+    setNewPhone("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <NewEntry
-        name={newName}
+        name={name}
         handleNameEntry={handleNameEntry}
+        phone={phone}
+        handlePhoneEntry={handlePhoneEntry}
         handleSubmit={handleSubmit}
       />
       <DisplayEntry entries={persons} />
@@ -29,14 +34,43 @@ const App = () => {
   );
 };
 
+// const Filter = () => {
+//   return (
+//     <form>
+//       <fieldset>
+//         <legend>Add a new</legend>
+//         <label>
+//           name: <input value={props.name} onChange={props.handleNameEntry} />
+//         </label>
+//         <button type="submit">add</button>
+//       </fieldset>
+//     </form>
+//   );
+// }
+
 const NewEntry = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <fieldset>
         <legend>Add a new</legend>
         <label>
-          name: <input value={props.name} onChange={props.handleNameEntry} />
+          name:{" "}
+          <input
+            value={props.name}
+            onChange={props.handleNameEntry}
+            required
+          />
         </label>
+        <br />
+        <label>
+          number:{" "}
+          <input
+            value={props.phone}
+            onChange={props.handlePhoneEntry}
+            required
+          />
+        </label>
+        <br />
         <button type="submit">add</button>
       </fieldset>
     </form>
@@ -49,7 +83,7 @@ const DisplayEntry = ({ entries }) => {
       <h2>Numbers</h2>
       <ul>
         {entries.map((entry) => (
-          <li key={entry.name}>{entry.name}</li>
+          <li key={entry.name}>{entry.name} {entry.phone}</li>
         ))}
       </ul>
     </div>

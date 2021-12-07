73f92e33b1008beebb8b61599601fb8e1807d538
brickwalker <artem.nedostup@gmail.com>
2021-12-07 15:49:16 +0000

    update to relative url and add proxy

--- part3/phonebook2/src/services/contacts.js
+++ part3/phonebook2/src/services/contacts.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://sheltered-sands-12002.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);

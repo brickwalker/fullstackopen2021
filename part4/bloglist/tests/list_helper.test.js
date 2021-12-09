73f7c2e6b9af4bf32a9f71988250f94141536126
brickwalker <artem.nedostup@gmail.com>
2021-12-09 17:33:50 +0000

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -21,3 +21,35 @@ describe("total likes", () => {
     expect(result).toBe(5000);
   });
 });
+
+describe("most likes", () => {
+  const listFavorites = [
+    {
+      title: "Karifood",
+      author: "Olga Kari",
+      url: "https://karifood.com/",
+      likes: 5000,
+      id: "61b1d4f6e937420f72deebff",
+    },
+    {
+      title: "Klopotenko recipes",
+      author: "Eugen Klopotenko",
+      url: "https://klopotenko.com/uk/",
+      likes: 197000,
+      id: "61b1d5c4e937420f72deec02",
+    },
+  ];
+
+  test("should be specific blog with most likes", () => {
+    const favBlog = {
+      title: "Klopotenko recipes",
+      author: "Eugen Klopotenko",
+      url: "https://klopotenko.com/uk/",
+      likes: 197000,
+      id: "61b1d5c4e937420f72deec02",
+    };
+
+    const result = listHelper.favoriteBlog(listFavorites);
+    expect(result).toEqual(favBlog);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,21 @@ const dummy = () => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, cur) => (sum += cur.likes), 0);
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((top, cur) => {
+    if (top.likes !== undefined) {
+      if (top.likes <= cur.likes) {
+        top = { ...cur };
+      }
+    } else {
+      top = { ...cur };
+    }
+    return top;
+  }, {});
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

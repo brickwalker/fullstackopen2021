09c4a2e1b4b3c0f71fc30063b214ec8e9fa90f66
brickwalker <artem.nedostup@gmail.com>
2021-12-09 18:29:56 +0000

    4.6*: helper functions and unit tests, step4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -53,3 +53,39 @@ describe("most likes", () => {
     expect(result).toEqual(favBlog);
   });
 });
+
+describe("prolific author", () => {
+  const allBlogs = [
+    {
+      title: "Karifood",
+      author: "Olga Kari",
+      url: "https://karifood.com/",
+      likes: 5000,
+      id: "61b1d4f6e937420f72deebff",
+    },
+    {
+      title: "Klopotenko recipes",
+      author: "Eugen Klopotenko",
+      url: "https://klopotenko.com/uk/",
+      likes: 197000,
+      id: "61b1d5c4e937420f72deec02",
+    },
+    {
+      title: "Klopotenko recipes",
+      author: "Eugen Klopotenko",
+      url: "https://klopotenko.com/",
+      likes: 197000,
+      id: "61b1fd67e46123ce60178331",
+    },
+  ];
+
+  test("should return author with most blogs", () => {
+    const prolificAuthor = {
+      author: "Eugen Klopotenko",
+      blogs: 2,
+    };
+
+    const result = listHelper.mostBlogs(allBlogs);
+    expect(result).toEqual(prolificAuthor);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = () => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, cur) => (sum += cur.likes), 0);
@@ -15,8 +17,21 @@ const favoriteBlog = (blogs) => {
   }, {});
 };
 
+const mostBlogs = (blogs) => {
+  const authorCount = _.countBy(blogs, (el) => el.author);
+  const prolificAuthor = _.maxBy(
+    Object.keys(authorCount),
+    (el) => authorCount[el]
+  );
+  return {
+    author: prolificAuthor,
+    blogs: authorCount[prolificAuthor],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };

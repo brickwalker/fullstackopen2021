9981d86b3c59d9f5d4b8662ffa217a3e89fdd54a
brickwalker <artem.nedostup@gmail.com>
2021-12-19 09:35:38 +0000

    5.14*: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "./Blog";
 
-describe("default display", () => {
-  const blog = {
-    title: "Kari4",
-    author: "Olga Kari3",
-    url: "https://karifood.com/3",
-    likes: 5003,
-    user: {
-      username: "brickwalker",
-      name: "Artem Nedostup",
-      id: "61bb36a267c8aa3148e34f7a",
-    },
-    id: "61bb880efcf9d3ac14d12232",
-  };
+const blog = {
+  title: "Kari4",
+  author: "Olga Kari3",
+  url: "https://karifood.com/3",
+  likes: 5003,
+  user: {
+    username: "brickwalker",
+    name: "Artem Nedostup",
+    id: "61bb36a267c8aa3148e34f7a",
+  },
+  id: "61bb880efcf9d3ac14d12232",
+};
 
+describe("default display", () => {
   test("should display title and author", () => {
     const component = render(<Blog blog={blog} displayMessage={() => {}} />);
     const blogHeader = component.getByText(`${blog.title} - ${blog.author}`);
@@ -31,5 +31,27 @@ describe("default display", () => {
     expect(likes).not.toBeVisible();
     const user = component.getByText(`${blog.user.name}`, { exact: false });
     expect(user).not.toBeVisible();
+  });
+});
+
+describe("expanded display", () => {
+  test("should display title and author", () => {
+    const component = render(<Blog blog={blog} displayMessage={() => {}} />);
+    const button = component.getByText("view");
+    fireEvent.click(button);
+    const blogHeader = component.getByText(`${blog.title} - ${blog.author}`);
+    expect(blogHeader).toBeVisible();
+  });
+
+  test("should display url, likes and user", () => {
+    const component = render(<Blog blog={blog} displayMessage={() => {}} />);
+    const button = component.getByText("view");
+    fireEvent.click(button);
+    const url = component.getByText(`http`, { exact: false });
+    expect(url).toBeVisible();
+    const likes = component.getByText(`likes`, { exact: false });
+    expect(likes).toBeVisible();
+    const user = component.getByText(`${blog.user.name}`, { exact: false });
+    expect(user).toBeVisible();
   });
 });

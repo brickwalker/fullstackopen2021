4237c1190d33d6ccdd1c51a504e25bf904c61597
brickwalker <artem.nedostup@gmail.com>
2021-12-20 13:06:53 +0000

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,13 @@
+const user = {
+  username: "brickwalker",
+  name: "Artem Nedostup",
+  password: "1a2b3c4d",
+};
+
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", "http://localhost:3003/api/users", user);
     cy.visit("http://localhost:3000");
   });
 
@@ -9,5 +16,23 @@ describe("Blog app", function () {
     cy.contains("username");
     cy.contains("password");
     cy.get("button");
+  });
+
+  describe("Login", function () {
+    it("should succeed with correct credentials", function () {
+      cy.contains("username").type(user.username);
+      cy.contains("password").type(user.password);
+      cy.get("button").click();
+      cy.contains("Artem Nedostup is logged in");
+    });
+
+    it("should fail with wrong credentials", function () {
+      cy.contains("username").type(user.username);
+      cy.contains("password").type("wrongPassword");
+      cy.get("button").click();
+      cy.get(".error")
+        .should("contain", "Login unsuccessful")
+        .and("have.css", "color", "rgb(139, 0, 0)");
+    });
   });
 });

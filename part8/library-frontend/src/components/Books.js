0393e8c4639ffb4e0ef7832304036fb84c574ab9
brickwalker <artem.nedostup@gmail.com>
2021-12-28 15:20:14 +0000

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const [genre, setGenre] = useState("all");
   const response = useQuery(ALL_BOOKS);
-  
+
   if (!props.show) {
     return null;
   }
 
   if (response.loading) {
-    return <div>loading...</div>
+    return <div>loading...</div>;
   }
 
-  const books = response.data.allBooks;
+  let books = response.data.allBooks;
+  const uniqueGenres = [];
+  books.forEach((book) =>
+    book.genres.forEach((genre) => {
+      if (!uniqueGenres.includes(genre)) {
+        uniqueGenres.push(genre);
+      }
+    })
+  );
+  uniqueGenres.sort();
+  if (genre !== "all") {
+    books = books.filter((book) => book.genres.includes(genre));
+  }
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        <button onClick={() => setGenre("all")}>all</button>
+        {uniqueGenres.map((genre) => (
+          <button key={genre} onClick={() => setGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
       <table>
         <tbody>
           <tr>

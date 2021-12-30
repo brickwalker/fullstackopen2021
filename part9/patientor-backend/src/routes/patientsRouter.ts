51eb760a44adaf9391b94e210b4a539e81717f6b
brickwalker <artem.nedostup@gmail.com>
2021-12-30 16:59:02 +0000

    9.12: Patientor backend, step5

--- part9/patientor-backend/src/routes/patientsRouter.ts
+++ part9/patientor-backend/src/routes/patientsRouter.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { patients } from "../services/patientsService";
+import { filteredPatients, addPatient } from "../services/patientsService";
+import { Patient } from "../types/types";
 
 const patientsRouter = express.Router();
 
 patientsRouter.get("/", (_req, res) => {
-  res.json(patients);
+  res.json(filteredPatients());
+});
+
+patientsRouter.post("/", (req, res) => {
+  const patientData = req.body as Omit<Patient, "id">;
+  const newPatient = addPatient(patientData);
+  res.json(newPatient);
 });
 
 export default patientsRouter;
--- part9/patientor-backend/src/services/patientsService.ts
+++ part9/patientor-backend/src/services/patientsService.ts
@@ -1,12 +1,27 @@
 import patientsData from "../../data/patients.json";
 import { Patient } from "../types/types";
+import { v1 as uuidv1 } from "uuid";
 
-export const patients: Omit<Patient, "ssn">[] = patientsData.map(
-  ({ id, name, dateOfBirth, gender, occupation }) => ({
+const patients: Patient[] = [...patientsData];
+
+export const filteredPatients = (): Omit<Patient, "ssn">[] =>
+  patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
     id,
     name,
     dateOfBirth,
     gender,
     occupation,
-  })
-);
+  }));
+
+export const addPatient = (
+  patientObject: Omit<Patient, "id">
+): Omit<Patient, "ssn"> => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const id = uuidv1() as string;
+  const newPatient = { ...patientObject, id };
+  patients.push(newPatient);
+  // Using destructuring to remove property
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { ssn, ...patientNoSsn } = newPatient;
+  return patientNoSsn;
+};
